1c3b430d79ee49521479de5d3a67845cda728f50
Yoma <codeyoma@gmail.com>
2022-05-24 22:47:09 +0900

    add sig_file

--- shell_sig.h
+++ shell_sig.h
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   shell.c                                            :+:      :+:    :+:   */
+/*   shell_sig.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkong <jkong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: yongmkim <codeyoma@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/20 20:34:05 by jkong             #+#    #+#             */
-/*   Updated: 2022/05/24 20:40:44 by yongmkim         ###   ########.fr       */
+/*   Created: 2022/05/24 20:41:14 by yongmkim          #+#    #+#             */
+/*   Updated: 2022/05/24 20:42:42 by yongmkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-#include <stdlib.h>
+#ifndef SHELL_SIG_H
+# define SHELL_SIG_H
 
-int	main(int argc, char *argv[])
-{
-	char	*rl;
-	int		exit_status;
+# include <signal.h>
 
-	(void)&argc;
-	(void)&argv;
-	exit_status = 0;
-	ft_signal();
-	while (1)
-	{
-		rl = readline("$ ");
-
-		free(rl);
-	}
-	return (exit_status);
-}
+#endif
